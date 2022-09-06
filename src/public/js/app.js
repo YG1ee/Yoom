a43d5fa1ed7b66663abe8982f0ddd240f7d73be1
YG1ee <fanfolderkim@gmail.com>
2022-09-06 21:21:48 +0900

    1 6 Chat Completed

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,25 @@
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
 const server_socket = new WebSocket(`ws://${window.location.host}`);
 
 server_socket.addEventListener("open", () => {
-  console.log("Connected to Server!");
+  console.log("Connected to Server! ✅");
 });
 
 server_socket.addEventListener("message", (message) => {
-  console.log("Just got this:", message.data, "from the server!");
+  console.log("New message:", message.data);
 });
 
 server_socket.addEventListener("close", () => {
-  console.log("Disconnected from Server.");
+  console.log("Disconnected from Server. ❌");
 });
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  server_socket.send(input.value);
+  input.value = "";
+};
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -16,11 +16,18 @@ const port = 3000;
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Listening on http://localhost:${port}`));
 
+const clnt_sockets = [];
+
 // and ws server at the same time
 const wss = new WebSocket.Server({ server });
 wss.on("connection", (client_socket) => {
-  console.log("Connected to a browser");
-  client_socket.on("close", () => console.log("Disconnected from a browser."));
-  client_socket.on("message", (message) => console.log(message));
+  console.log("Connected to a browser ✅");
+  clnt_sockets.push(client_socket);
+  client_socket.on("close", () =>
+    console.log("Disconnected from a browser. ❌")
+  );
+  client_socket.on("message", (message) =>
+    clnt_sockets.forEach((c_socket) => c_socket.send(message.toString("utf-8")))
+  );
   client_socket.send("hello!!!");
 });
